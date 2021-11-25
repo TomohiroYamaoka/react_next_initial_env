c2d685f4eadd4d117ed37564384eab975ce7a7d2
山岡智裕 <>
2021-11-25 09:17:47 +0900

    設定ファイル追加

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-  reactStrictMode: true,
-}
+/* eslint-disable
+   @typescript-eslint/no-var-requires
+*/
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+  },
+})
